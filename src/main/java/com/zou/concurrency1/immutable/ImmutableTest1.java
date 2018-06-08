432476687fb86a30ce5fffb2596a4d827b069561
zoulvzhou <940933073@qq.com>
2018-06-08 10:35:43 +0800

    不可变Map

--- src/main/java/com/zou/concurrency1/immutable/ImmutableTest1.java
+++ src/main/java/com/zou/concurrency1/immutable/ImmutableTest1.java
@@ -2,29 +2,28 @@ package com.zou.concurrency1.immutable;
 
 import com.google.common.collect.Maps;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class ImmutableTest1 {
 
 
-    private final static Integer a = 1;
-
-    private final static String b = "2";
-
-    private final static Map<Integer, Integer> map = Maps.newHashMap();
+    private static Map<Integer, Integer> map = Maps.newHashMap();
 
     static {
 
         map.put(1, 2);
         map.put(3, 4);
         map.put(5, 6);
+        map = Collections.unmodifiableMap(map);
+
 
     }
 
     public static void main(String[] args) {
 
-        // a = 2; //不允许
-        //  map = Maps.newHashMap(); //不允许
+        map.put(1, 3); //  会抛出异常,不可变的map
+        System.out.println(map.get(1));
 
     }
 

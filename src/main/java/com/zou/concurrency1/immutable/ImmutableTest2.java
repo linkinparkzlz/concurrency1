b87d11ea7e9d51b1e1a64e6dbf60ba633574d71e
zoulvzhou <940933073@qq.com>
2018-06-08 10:43:10 +0800

    不可变集合

--- src/main/java/com/zou/concurrency1/immutable/ImmutableTest2.java
+++ src/main/java/com/zou/concurrency1/immutable/ImmutableTest2.java
@@ -1,5 +1,8 @@
 package com.zou.concurrency1.immutable;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
@@ -7,25 +10,40 @@ import java.util.Map;
 public class ImmutableTest2 {
 
 
-    private final static Integer a = 1;
+    private final static ImmutableList<Integer> list = ImmutableList.of(1, 2, 3);
 
-    private final static String b = "2";
+    private final static ImmutableSet set = ImmutableSet.copyOf(list);
 
-    private final static Map<Integer, Integer> map = Maps.newHashMap();
-
-    static {
-
-        map.put(1, 2);
-        map.put(3, 4);
-        map.put(5, 6);
-
-    }
+    private final static ImmutableMap<Integer, Integer> map = ImmutableMap.of(1, 2, 3, 4, 5, 6, 7, 8);
 
     public static void main(String[] args) {
 
-        // a = 2; //不允许
-        //  map = Maps.newHashMap(); //不允许
-
     }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

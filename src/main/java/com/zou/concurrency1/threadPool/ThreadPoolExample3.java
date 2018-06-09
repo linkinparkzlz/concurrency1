22762f6fa4d7a1843c2c1496b29e5750d785b143
zoulvzhou <940933073@qq.com>
2018-06-09 21:08:23 +0800

    线程池 newSingleThreadExecutor 实例

---
+++ src/main/java/com/zou/concurrency1/threadPool/ThreadPoolExample3.java
@@ -0,0 +1,61 @@
+package com.zou.concurrency1.threadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolExample3 {
+
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        for (int i = 0; i < 10; i++) {
+
+            final int index = i;
+
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("线程：" + Thread.currentThread().getName() + "  " + index);
+                }
+            });
+        }
+
+        executorService.shutdown();
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
